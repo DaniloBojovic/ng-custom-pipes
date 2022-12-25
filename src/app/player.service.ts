9f243347b0390cd9d4da2558741f86b3891de96f
Danilo <danilobojovicdb@gmail.com>
2022-12-25 17:32:41 +0100

    Refactoring

--- src/app/player.service.ts
+++ src/app/player.service.ts
@@ -2,13 +2,16 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { catchError, map, tap } from 'rxjs/operators';
+import { environment } from 'src/environments/environment';
 import { Player } from './player';
+
+const BASE_PATH = environment.basePath;
 
 @Injectable({
   providedIn: 'root',
 })
 export class PlayerService {
-  private playersUrl = 'api/players'; // URL to web api
+  private playersUrl = `${BASE_PATH}/players`; // URL to web api
   players$ = this.http
     .get<Player[]>(this.playersUrl)
     .pipe(catchError((error) => of([])));
